454d9b29b9de34979a119181912aed2ffed4862a
Alex Naidovich <smiler.work@gmail.com>
2018-05-23 18:20:39 +0300

    footer link added

--- assets/js/components/AudioPlayer.js
+++ assets/js/components/AudioPlayer.js
@@ -1,19 +1,8 @@
 const audio = document.querySelector('#audio');
 const control = document.querySelector('#audio-control');
-const play = control.querySelector('#audio-play'),
-      stop = control.querySelector('#audio-stop');
 
 control.addEventListener('click', e => {
   e.preventDefault();
   control.classList.toggle('audio-active');
-  let target = e.target;
-  if (target.getAttribute('data-audio') === 'play') {
-    audio.play();
-    play.classList.remove('unhidden');
-    stop.classList.add('unhidden');
-  } else {
-    audio.pause();
-    stop.classList.remove('unhidden');
-    play.classList.add('unhidden');
-  }
+  audio.paused ? audio.play() : audio.pause();
 })
